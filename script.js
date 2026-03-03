1c61f41168d66ca2502e91cfa95dc47c41459beb
tabrejsiddi <139753435+tabrejsiddi@users.noreply.github.com>
2026-03-03 14:07:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,33 @@
 function createTable() {
-    //Write your code here
+  const table = document.getElementById("myTable");
+
   
+  const rn = window.prompt("Input number of rows");
+  const cn = window.prompt("Input number of columns");
+
+  const rows = parseInt(rn, 10);
+  const cols = parseInt(cn, 10);
+
+
+  if (isNaN(rows) || isNaN(cols)) return;
+
+  if (rows <= 0 || cols <= 0) {
+    alert("Invalid input");
+    return;
+  }
+
+  
+  table.innerHTML = "";
+
+  for (let i = 0; i < rows; i++) {
+    const tr = document.createElement("tr");
+
+    for (let j = 0; j < cols; j++) {
+      const td = document.createElement("td");
+      td.innerHTML = `Row-${i} Column-${j}`;
+      tr.appendChild(td);
+    }
+
+    table.appendChild(tr);
+  }
 }
